fdc4054310edf599ce797da43df073dda5c92e01
alfa9317 <josealfredotg@gmail.com>
2019-10-15 09:36:48 -0500

    display and append new buttons to the DOM with the text field attribute

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,22 @@
-document.write("Hola mundo");
+var categories = ["superman","pikachu","ferrari","crazy doctor","cats","dogs","love","breaking bad"];
 
-$("#title").html("JQuery");
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < categories.length; i++) {
+      var a = $("<button>");
+      a.addClass("gifs-btn");
+      a.attr("data-name", categories[i]);
+      a.text(categories[i]);
+      $("#buttons-view").append(a);
+    }
+  }
+
+  $("#add-gifCat").on("click", function(event) {
+    event.preventDefault();
+    var gifCategory = $("#gifCat-input").val().trim();
+    categories.push(gifCategory);
+    renderButtons();
+    $("#gifCat-input").val("");
+  });
+
+  renderButtons();
